21f61624ee32b3295001abb1c677f2fbc1bd79e9
freepol-bit <freepol@gmail.com>
2026-04-13 14:23:00 +0900

    Update main.py
    test

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 @app.get("/{skey}")
-def read_skey(skey: str):
-    return {"message": f"Hello {skey}"}
+def get_data(skey: str):
+    url = f"https://sd.wips.co.kr/wipslink/doc/docContJson.wips?skey={skey}&tabGb=DS"
+    
+    try:
+        resp = requests.get(url, timeout=10)
+        resp.raise_for_status()
+        return resp.json()  # JSON 그대로 반환
+
+    except Exception as e:
+        return {"error": str(e)}
